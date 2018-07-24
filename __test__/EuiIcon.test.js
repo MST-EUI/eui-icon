767bafd29aaf0959e0c8fdd01058c9ab1fae0c3a
yukap <wb-jwr216045@alibaba-inc.com>
2018-07-24 21:08:22 +0800

    remove useless debugger

--- __test__/EuiIcon.test.js
+++ __test__/EuiIcon.test.js
@@ -10,7 +10,6 @@ test('EuiIcon should render correctly', () => {
 });
 
 test('EuiIcon props', () => {
-  // Render a checkbox with label in the document
   const euiIcon = shallow(<EuiIcon className="test" />);
   expect(euiIcon.find('.eui-icon')).toBeTruthy();
 });
